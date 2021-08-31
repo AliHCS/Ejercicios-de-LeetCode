8f289ca4392101fbe94ee5ce482906ad1e4ac7ae
Gustavo Ali <aligusali@gmail.com>
2021-08-30 21:20:59 -0500

    Se agregaron 2 ejercicios mas de nivel Facil

--- 482. License Key Formatting.py
+++ 482. License Key Formatting.py
@@ -1,3 +1,17 @@
+# Descripcion(Ingles)
+# You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. 
+# The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+# We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. 
+# Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
+# Return the reformatted license key.
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Descripcion
+# Se le proporciona una clave de licencia representada como una cadena que consta solo de caracteres alfanuméricos y guiones. La cadena está separada en n + 1 grupos por n guiones. 
+# También se le da un número entero k.
+# Queremos reformatear la cadena s de modo que cada grupo contenga exactamente k caracteres, excepto el primer grupo, que podría ser más corto que k pero debe contener al menos un carácter.
+#  Además, debe haber un guión insertado entre dos grupos y debe convertir todas las letras minúsculas a mayúsculas.
+# Devuelva la clave de licencia reformateada. 
+
 class Solution:
     def licenseKeyFormatting(self, S: str, K: int) -> str:
         S=S.replace("-","").upper() # Eliminamos los "-" y convertimos en mayusculas
